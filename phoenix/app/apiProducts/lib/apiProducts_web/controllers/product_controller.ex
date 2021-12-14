e8735817fa57cce6bcfe66d4128c24abc11c6465
mellyssaStephanny <mellyssah.mendes@live.com>
2021-12-14 15:47:38 -0300

    utilize plug to find product by id

--- phoenix/app/apiProducts/lib/apiProducts_web/controllers/product_controller.ex
+++ phoenix/app/apiProducts/lib/apiProducts_web/controllers/product_controller.ex
@@ -1,8 +1,11 @@
 defmodule ApiProductsWeb.ProductController do
   use ApiProductsWeb, :controller
-
+  import ApiProductsWeb.PlugId
+  
   alias ApiProducts.Catalog
   alias ApiProducts.Catalog.Product
+
+  plug :get_product when action in [:show, :update, :delete]
 
   action_fallback ApiProductsWeb.FallbackController
 
@@ -13,32 +16,35 @@ defmodule ApiProductsWeb.ProductController do
 
 
   def create(conn, %{"product" => product_params}) do
-    with {:ok, %Product{} = product} <- Catalog.create_product(product_params) do
-      conn
-      |> put_status(:created)
-      |> render("show.json", product: product)
+    case Catalog.create_product(product_params) do
+      {:ok, %Product{} = product} ->
+        conn
+        |> put_status(:created)
+        |> render("show.json", product: product)
+      error ->
+        error 
     end
   end
 
 
   def show(conn, %{"id" => id}) do
-    product = Catalog.get_product!(id)
-    render(conn, "show.json", product: product)
+    case conn.assigns[:get_product] do
+      {:ok, %Product{} = product} -> render(conn, "show.json", product: product)
+      error -> error
+    end
   end
   
   
   def update(conn, %{"id" => id, "product" => product_params}) do
-    product = Catalog.get_product!(id)
-
-    with {:ok, %Product{} = product} <- Catalog.update_product(product, product_params) do
-      render(conn, "show.json", product: product)
+    with {:ok, %Product{} = product} <- conn.assigns[:get_product],
+         {:ok, %Product{} = updated_product} <- Catalog.update_product(product, product_params) do
+      render(conn, "show.json", product: updated_product)
     end
   end
-
+    
 
   def delete(conn, %{"id" => id}) do
-    product = Catalog.get_product!(id)
-
+    with {:ok, %Product{} = product} <- conn.assigns[:get_product]
     with {:ok, %Product{}} <- Catalog.delete_product(product) do
       send_resp(conn, :no_content, "")
     end
