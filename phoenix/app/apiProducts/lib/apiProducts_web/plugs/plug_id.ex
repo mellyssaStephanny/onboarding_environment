570961670572e3c71e599c12499cb8c5e5e555ed
mellyssaStephanny <mellyssah.mendes@live.com>
2021-12-14 15:33:22 -0300

    add plug find product by id

---
+++ phoenix/app/apiProducts/lib/apiProducts_web/plugs/plug_id.ex
@@ -0,0 +1,16 @@
+defmodule ApiProductsWeb.Plugs.PlugId do
+  import Plug.Conn
+
+  def get_product(conn, _opts), do: product_id(conn, conn.params["id"])
+  
+  defp product_by_id(conn, nil), do: assign(conn, :get_product, {:error, :bad_request})
+
+  defp product_by_id(conn, id) do
+    with product <- Catalog.get_product!(id),
+         true <- product != nil do
+      assign(conn, :get_product, {:ok, product})
+    else
+      _ -> assign(conn, :get_product, {:error, :not_found})
+    end
+  end
+end
