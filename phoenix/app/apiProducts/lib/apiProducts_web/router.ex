9d09f6be5fd251ede668a3437ab82dba9dc2e7a7
mellyssaStephanny <mellyssah.mendes@live.com>
2021-12-15 16:34:24 -0300

    remove route

--- phoenix/app/apiProducts/lib/apiProducts_web/router.ex
+++ phoenix/app/apiProducts/lib/apiProducts_web/router.ex
@@ -7,25 +7,6 @@ defmodule ApiProductsWeb.Router do
 
   scope "/", ApiProductsWeb do
     pipe_through :api
-    
     resources "/products", ProductController, only: [:index, :show, :create, :update, :delete]
   end
-
-  # Enables LiveDashboard only for development
-  #
-  # If you want to use the LiveDashboard in production, you should put
-  # it behind authentication and allow only admins to access it.
-  # If your application does not have an admins-only section yet,
-  # you can use Plug.BasicAuth to set up some basic authentication
-  # as long as you are also using SSL (which you should anyway).
-  if Mix.env() in [:dev, :test] do
-    import Phoenix.LiveDashboard.Router
-
-    scope "/" do
-      pipe_through [:fetch_session, :protect_from_forgery]
-      live_dashboard "/dashboard", metrics: ApiProductsWeb.Telemetry
-      resources "/products", ProductController, except: [:new, :edit]
-    end
-  end
 end
-
