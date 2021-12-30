0c064d7abbc718406097ebe97de9dbd90c496ceb
mellyssaStephanny <mellyssah.mendes@live.com>
2021-12-30 14:44:55 -0300

    remove spaces

--- phoenix/app/apiProducts/config/test.exs
+++ phoenix/app/apiProducts/config/test.exs
@@ -12,8 +12,6 @@ config :apiProducts, ApiProducts.Repo,
   hostname: "localhost",
   pool: Ecto.Adapters.SQL.Sandbox
   
-
-
 # We don't run a server during test. If one is required,
 # you can enable the server option below.
 config :apiProducts, ApiProductsWeb.Endpoint,
