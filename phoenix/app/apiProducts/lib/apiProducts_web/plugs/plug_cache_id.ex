461271e2054f9cecfcf12128157e0ca5b34bca82
mellyssaStephanny <mellyssah.mendes@live.com>
2022-01-12 15:59:38 -0300

    refactoring plug

--- phoenix/app/apiProducts/lib/apiProducts_web/plugs/plug_cache_id.ex
+++ phoenix/app/apiProducts/lib/apiProducts_web/plugs/plug_cache_id.ex
@@ -11,18 +11,18 @@ defmodule ApiProductsWeb.Plugs.PlugCacheId do
   # plug for cache redis 
   def get_cache(conn, id) do
     id = conn.params["id"]
-    case Cache.get(conn, id) do
-      nil ->
-        conn 
-        |> halt()
-        |> send_resp(:not_found, "")
-      product -> 
-        assing(conn, :product, product)
+    case Cache.get(id) do
+      {:ok, product} ->
+        assign(conn, :product, product)
       _ ->
-        case Catalog.get_product(id) do
-          Cache.set(id, product)
+      case Catalog.get_product(id) do
+        nil ->
+          conn
+          |> halt()
+          |> send_resp(:not_found, "")
+        product ->
           assign(conn, :product, product)
-        _ ->
-    end 
-  end 
+      end
+    end
+  end
 end 
