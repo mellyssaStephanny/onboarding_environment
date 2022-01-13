7160f0b2f0042fc0203ddcd43d3e1e62113ab9a6
mellyssaStephanny <mellyssah.mendes@live.com>
2022-01-13 13:45:10 -0300

    add style guide

--- phoenix/app/apiProducts/lib/apiProducts_web/plugs/plug_cache_id.ex
+++ phoenix/app/apiProducts/lib/apiProducts_web/plugs/plug_cache_id.ex
@@ -11,15 +11,18 @@ defmodule ApiProductsWeb.Plugs.PlugCacheId do
   # plug for cache redis 
   def get_cache(conn, _id) do
     id = conn.params["id"]
+
     case Cache.get(id) do
       {:ok, product} ->
         assign(conn, :product, product)
+      
       _ ->
       case Catalog.get_product(id) do
         nil ->
           conn
           |> halt()
           |> send_resp(:not_found, "")
+          
         product ->
           assign(conn, :product, product)
       end
