2e90880d51c4c2883c63fc97cf96808ce23dec21
mellyssaStephanny <mellyssah.mendes@live.com>
2022-01-17 19:20:33 -0300

    refactor cache

--- phoenix/app/apiProducts/lib/apiProducts/cache.ex
+++ phoenix/app/apiProducts/lib/apiProducts/cache.ex
@@ -16,10 +16,10 @@ defmodule ApiProducts.Cache do
     |> Base.encode16()
   end
 
+  defp decode({:ok, nil}), do: {:error, "Key not found!"}
+
   defp decode({:ok, val}) do
     {:ok, bin} = Base.decode16(val)
     {:ok, :erlang.binary_to_term(bin)}
   end
-
-  defp decode({:ok, nil}), do: {:error, "Key not found"}
 end
