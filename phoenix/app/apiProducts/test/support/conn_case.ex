a62d715e5c0b05128c5bc96867189510ef51f9c2
mellyssaStephanny <mellyssah.mendes@live.com>
2022-01-26 09:14:49 -0300

    redis cache test

--- phoenix/app/apiProducts/test/support/conn_case.ex
+++ phoenix/app/apiProducts/test/support/conn_case.ex
@@ -32,12 +32,6 @@ defmodule ApiProductsWeb.ConnCase do
   end
 
   setup tags do
-    :ok = Ecto.Adapters.SQL.Sandbox.checkout(ApiProducts.Repo)
-
-    unless tags[:async] do
-      Ecto.Adapters.SQL.Sandbox.mode(ApiProducts.Repo, {:shared, self()})
-    end
-
     {:ok, conn: Phoenix.ConnTest.build_conn()}
   end
 end
