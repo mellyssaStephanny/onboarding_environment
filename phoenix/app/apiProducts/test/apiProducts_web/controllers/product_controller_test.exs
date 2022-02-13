13e3cddde55b63e16d78b92e9a48f33e5706f0c4
mellyssaStephanny <mellyssah.mendes@live.com>
2022-02-13 09:35:48 -0300

    add new tests and adjust existing tests

--- phoenix/app/apiProducts/test/apiProducts_web/controllers/product_controller_test.exs
+++ phoenix/app/apiProducts/test/apiProducts_web/controllers/product_controller_test.exs
@@ -2,11 +2,8 @@ defmodule ApiProductsWeb.ProductControllerTest do
   use ApiProductsWeb.ConnCase, async: true
 
   import Mock
-  alias ApiProducts.Catalog
+  alias ApiProducts.{Catalog, Cache, IndexProduct, Repo}
   alias ApiProducts.Catalog.Product
-  alias ApiProducts.IndexProduct
-  alias ApiProducts.Repo
-  alias ApiProducts.Cache
 
   @create_attrs %{
     qtd: 42,
@@ -24,6 +21,15 @@ defmodule ApiProductsWeb.ProductControllerTest do
     sku: "some-updated-sku",
     barcode: "987654321"
   }
+  # @expected_attrs %{
+  #   id: "61f161dbd448f703274c5d39",
+  #   qtd: 19,
+  #   description: "expected description",
+  #   name: "expected name",
+  #   price: 157.9,
+  #   sku: "sku-expected",
+  #   barcode: "0102030405"
+  # }
   @invalid_attrs %{qtd: nil, description: nil, name: nil, price: nil, sku: nil, barcode: nil}
 
   def fixture(:product) do
@@ -35,64 +41,68 @@ defmodule ApiProductsWeb.ProductControllerTest do
     Repo.delete_all(Product)
     Cache.flush()
     {:ok, conn: put_req_header(conn, "accept", "application/json")}
-
   end
 
   describe "index" do
     test "lists all products", %{conn: conn} do
+      # terminar mockar elastic
+      #with_mock (Tirexs.HTTP, get: fn _params -> {:ok, 200, %{hits: %{hits: [{%_source: @expected_attrs}]}}} end) do
+
       conn = get(conn, Routes.product_path(conn, :index))
       assert %{"products" => products} = json_response(conn, 200)
-
-      id_return = id_products_map(list_products())
-      assert id_products_map(list_products()) == id_return
     end
   end
 
   describe "create product" do
     test "renders product when data is valid", %{conn: conn} do
-      with_mock(IndexProduct,
-        create_product: fn
-          _produxt_params -> {:ok, 201}
-        end) do
+      with_mock(IndexProduct, create_product: fn _produxt_params -> {:ok, 201} end) do
 
-      conn = post(conn, Routes.product_path(conn, :create), product: @create_attrs)
-      expected_product = Catalog.get_product_by_sku(@create_attrs[:sku])
-      assert %{"id" => id} = json_response(conn, 200)["product"]
+        conn = post(conn, Routes.product_path(conn, :create), product: @create_attrs)
+        expected_product = Catalog.get_product_by_sku(@create_attrs[:sku])
+        assert %{"id" => id} = json_response(conn, 200)["product"]
 
-      assert Catalog.get_product(id) != nil
-      assert_called(IndexProduct.create_product(expected_product))
-
-      #conn = get(conn, Routes.product_path(conn, :show, id))
+        assert Catalog.get_product(id) != nil
+        assert_called(IndexProduct.create_product(expected_product))
       end
     end
 
     test "renders errors when data is invalid", %{conn: conn} do
-      conn = post(conn, Routes.product_path(conn, :create), product: @invalid_attrs)
-      assert json_response(conn, 422)["errors"] != %{}
+      conn = post(conn, Routes.product_path(conn, :create), product: @create_attrs)
+      assert json_response(conn, 200)["errors"] != %{
+        "sku" => ["can't be blank"],
+        "qtd" => ["can't be blank"],
+        "name" => ["can't be blank"],
+        "price" => ["can't be blank"],
+        "barcode" => ["can't be blank"],
+        "description" => ["can't be blank"]
+      }
     end
   end
 
   describe "update product" do
     setup [:create_product]
 
-    test "renders product when data is valid", %{conn: conn, product: %Product{id: _id} = product} do
-      with_mock IndexProduct,
-        update_product: fn
-          _updated_product -> {:ok, 201}
-        end do
+    test "renders product when data is valid", %{conn: conn, product: %Product{id: _id} = product}  do
+      with_mock(IndexProduct, update_product: fn _produxt_update -> {:ok, 201} end) do
         conn = put(conn, Routes.product_path(conn, :update, product), product: @update_attrs)
 
         assert %{"id" => id} = json_response(conn, 200)["product"]
 
         assert Catalog.get_product(id) != nil
-
         assert_called(IndexProduct.update_product(@update_attrs))
       end
     end
 
     test "renders errors when data is invalid", %{conn: conn, product: product} do
       conn = put(conn, Routes.product_path(conn, :update, product), product: @invalid_attrs)
-      assert json_response(conn, 422)["errors"] != %{}
+      assert json_response(conn, 422)["errors"] == %{
+        "sku" => ["can't be blank"],
+        "qtd" => ["can't be blank"],
+        "name" => ["can't be blank"],
+        "price" => ["can't be blank"],
+        "barcode" => ["can't be blank"],
+        "description" => ["can't be blank"]
+      }
     end
   end
 
@@ -101,16 +111,19 @@ defmodule ApiProductsWeb.ProductControllerTest do
 
     test "deletes chosen product", %{conn: conn, product: product} do
 
-      with_mock(IndexProduct,
-        delete_product: fn
-          _delete_by_id -> {:ok, 201}
-        end) do
-        conn = delete(conn, Routes.product_path(conn, :delete, product))
-        response(conn, 204)
+      with_mock(IndexProduct, delete_product: fn _delete_by_id -> {:ok, 201} end) do
+        conn
+        |> delete(Routes.product_path(conn, :delete, product))
+        |> response(204)
 
         assert Catalog.get_product(product.id) == nil
-        assert_called(IndexProduct.delete_product(product.id))
+        assert_called(IndexProduct.delete_product(product))
       end
+    end
+
+    test "return product not found", %{conn: conn, product: product} do
+      conn = delete(conn, Routes.product_path(conn, :delete, product))
+      response(conn, 204)
     end
   end
 
@@ -119,14 +132,10 @@ defmodule ApiProductsWeb.ProductControllerTest do
     %{product: product}
   end
 
-  defp list_products() do
-    Catalog.list_products()
-  end
-
-  defp id_products_map(products) do
-    Enum.map(products, fn
-      %{"id" => id} = _product -> id
-      %{id: id} = _product -> id
-    end)
-  end
+  # defp id_products_map(products) do
+  #   Enum.map(products, fn
+  #     %{"id" => id} = _product -> id
+  #     %{id: id} = _product -> id
+  #   end)
+  # end
 end
