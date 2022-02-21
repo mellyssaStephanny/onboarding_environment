87bb9a2f61c43492eae9c979cad4a35c4e920f24
mellyssaStephanny <mellyssah.mendes@live.com>
2022-02-21 08:54:52 -0300

    add match for error 201

--- phoenix/app/apiProducts/lib/apiProducts_web/controllers/fallback_controller.ex
+++ phoenix/app/apiProducts/lib/apiProducts_web/controllers/fallback_controller.ex
@@ -15,12 +15,12 @@ defmodule ApiProductsWeb.FallbackController do
     render(conn, "show.json", product: product)
   end
 
-  def call(conn, {:ok, :no_content}) do
-    send_resp(conn, :no_content, "")
-  end
-
   def call(conn, {:ok, :created}) do
-    send_resp(conn, 201, "")
+    conn
+    |> put_status(:created)
+    |> render(:"201")
+    |> halt()
+    #|> render("show.json", product: product)
   end
 
   # This clause handles errors returned by Ecto's insert/update/delete.
