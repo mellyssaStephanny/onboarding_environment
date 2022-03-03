a967ce50fe04c14f832d870fe994439e89ba9e4c
mellyssaStephanny <mellyssah.mendes@live.com>
2022-03-03 09:18:55 -0300

    remove pool Ecto.Adapters.SQL

--- phoenix/app/apiProducts/config/test.exs
+++ phoenix/app/apiProducts/config/test.exs
@@ -9,8 +9,8 @@ use Mix.Config
 # Configure your database
 config :apiProducts, ApiProducts.Repo,
   database: "apiproducts_dev_test",
-  hostname: "localhost",
-  pool: Ecto.Adapters.SQL.Sandbox
+  hostname: "localhost"
+  #pool: Ecto.Adapters.SQL.Sandbox
 
 # We don't run a server during test. If one is required,
 # you can enable the server option below.
